60e48b68abd26f2c23d7b52e8f6f7d1e870f9ed1
marieb44 <marie.berger2022@campus-eni.fr>
2023-02-01 17:24:48 +0100

    TP Bucket-list => Doctrine et données

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,20 +11,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/', name: '_list')]
-    public function list(): Response
+    public function list(
+        WishRepository $wishRepository
+    ): Response
     {
         $date = new \DateTime();
+        //$wishes = $wishRepository->findAll();
+        $wishes = $wishRepository->findBy([], ["dateCreated" => "DESC"], null, 0);
         return $this->render('wish/list.html.twig', [
-            'today' => $date
+            'today' => $date,
+            'wishes' => $wishes
         ]);
     }
 
     #[Route('/detail/{id}', name: '_detail')]
-    public function detail(int $id): Response
+    public function detail(
+        int            $id,
+        WishRepository $wishRepository
+    ): Response
     {
         $date = new \DateTime();
+        $wish = $wishRepository->findOneBy(["id" => $id]);
         return $this->render('wish/detail.html.twig', [
-            'today' => $date
+            'today' => $date,
+            'wish' => $wish
         ]);
     }
 }
